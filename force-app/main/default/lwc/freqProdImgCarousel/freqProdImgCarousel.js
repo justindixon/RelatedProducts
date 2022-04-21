2a35f5e5b9a92272b3bf9f4e801061460ff0b650
Justin DIxon <justindixon91@outlook.com>
2022-04-21 23:15:46 +1000

    linting

--- force-app/main/default/lwc/freqProdImgCarousel/freqProdImgCarousel.js
+++ force-app/main/default/lwc/freqProdImgCarousel/freqProdImgCarousel.js
@@ -8,24 +8,15 @@ export default class FreqProdImgCarousel extends LightningElement {
     error;
     rendered = false;
     renderedCallback() {
- 
-        console.log('Hello');
-        console.log(this.recordId);
         if(!this.rendered) {
             this.rendered = true;
             getFrequentProducts({product2Id: this.recordId})
             .then(
                 result => {
-                    console.log(result);
-                    console.log(result.length);
                     let results = []; 
-                    console.log(results);
                     for(let i = 0; i < result.length; i++) {
-                        console.log(i);
                         results[i] = {...result[i], URLlink: window.location.origin + '/lightning/r/Product2/' + result[i].Id + '/view'};
                         results[i] = {...result[i], srcURL: myContentAsset + '/' + result[i].ProductCode + '.jpg'};
-                        console.log(results);
-                        console.log(result[i]);
                     }
                     this.products = results;
             })
